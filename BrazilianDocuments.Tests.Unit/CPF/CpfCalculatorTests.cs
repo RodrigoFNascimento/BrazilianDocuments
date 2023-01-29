4a39b01d0764ddd5273a02040d97254de7cfae46
RodrigoFNascimento <rodrigofn0@hotmail.com>
2023-01-29 11:19:09 -0300

    test: Turn Theory into Fact

--- BrazilianDocuments.Tests.Unit/CPF/CpfCalculatorTests.cs
+++ BrazilianDocuments.Tests.Unit/CPF/CpfCalculatorTests.cs
@@ -6,15 +6,12 @@ namespace BrazilianDocuments.Tests.Unit.CPF;
 
 public class CpfCalculatorTests
 {
-    [Theory]
-    [InlineData(new byte[] { 0, 4, 5, 5, 1, 5, 3, 8, 5 }, 0)]
-    [InlineData(new byte[] { 8, 7, 0, 3, 7, 6, 3, 4, 0 }, 4)]
-    [InlineData(new byte[] { 8, 3, 5, 4, 0, 4, 0, 6, 0 }, 7)]
-    [InlineData(new byte[] { 6, 1, 3, 1, 3, 4, 8, 7, 0 }, 7)]
-    [InlineData(new byte[] { 1, 1, 1, 4, 4, 4, 7, 7, 7 }, 3)]
-    public void GetFirstVerificationDigit_ShouldCalculateFirstVerificationDigit_WhenGivenACpf(byte[] a, byte expected)
+    [Fact]
+    public void GetFirstVerificationDigit_ShouldCalculateFirstVerificationDigit_WhenGivenACpf()
     {
         // Arrange
+        var a = new byte[] { 8, 7, 0, 3, 7, 6, 3, 4, 0 };
+        byte expected = 4;
 
         // Act
         var digit = CpfCalculator.GetFirstVerificationDigit(a);
@@ -34,15 +31,12 @@ public class CpfCalculatorTests
         Assert.Throws<ArgumentException>(() => CpfCalculator.GetFirstVerificationDigit(Array.Empty<byte>()));
     }
 
-    [Theory]
-    [InlineData(new byte[] { 0, 4, 5, 5, 1, 5, 3, 8, 5, 0 }, 7)]
-    [InlineData(new byte[] { 8, 7, 0, 3, 7, 6, 3, 4, 0, 4 }, 2)]
-    [InlineData(new byte[] { 8, 3, 5, 4, 0, 4, 0, 6, 0, 7 }, 7)]
-    [InlineData(new byte[] { 6, 1, 3, 1, 3, 4, 8, 7, 0, 7 }, 4)]
-    [InlineData(new byte[] { 1, 1, 1, 4, 4, 4, 7, 7, 7, 3 }, 5)]
-    public void GetSecondVerificationDigit_ShouldCalculateSecondVerificationDigit_WhenGivenACpf(byte[] a, byte expected)
+    [Fact]
+    public void GetSecondVerificationDigit_ShouldCalculateSecondVerificationDigit_WhenGivenACpf()
     {
         // Arrange
+        var a = new byte[] { 8, 7, 0, 3, 7, 6, 3, 4, 0, 4 };
+        byte expected = 2;
 
         // Act
         var digit = CpfCalculator.GetSecondVerificationDigit(a);
